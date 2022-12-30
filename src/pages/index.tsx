5a4fa888b10d8e85d4e7b5914ceb32b3a1cb1e85
Leonardo M. Primieri <leonardo.primieri@sof.to>
2022-12-30 19:48:30 -0300

    chore: add lint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import { Heading } from "@ignite-ui/react";
+import { Heading } from '@ignite-ui/react'
 
 export default function Home() {
-  return <Heading>Hello cu</Heading>;
+  return <Heading>Hello cu</Heading>
 }
