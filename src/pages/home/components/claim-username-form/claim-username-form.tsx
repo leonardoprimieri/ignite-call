cd76aa1c78e4ed570e6a1bb57bda9bcb61fd5b3d
Leonardo M. Primieri <leonardo.primieri@sof.to>
2022-12-30 22:21:07 -0300

    feat: validate user input

--- src/pages/home/components/claim-username-form/claim-username-form.tsx
+++ src/pages/home/components/claim-username-form/claim-username-form.tsx
@@ -1,15 +1,53 @@
-import { TextInput, Button } from '@ignite-ui/react'
+import { TextInput, Button, Text } from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
-import { Form } from './claim-username-form-styles'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { Form, FormAnnotation } from './claim-username-form-styles'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const claimUsernameFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: 'O usuário precisa ter pelo menos 3 letras.' })
+    .regex(/^(a-z\\-]+)$/i, {
+      message: 'O usuário pode ter apenas letras e hifens.',
+    })
+    .transform((value) => value.toLowerCase()),
+})
+
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
 
 export function ClaimUsernameForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ClaimUsernameFormData>({
+    resolver: zodResolver(claimUsernameFormSchema),
+  })
+
+  const handleClaimUsername = async (data: ClaimUsernameFormData) => {
+    console.log(data)
+  }
+
   return (
-    <Form as="form">
-      <TextInput size="sm" prefix="ignite.com/" placeholder="seu-usuário" />
-      <Button size="sm" type="submit">
-        Reservar
-        <ArrowRight />
-      </Button>
-    </Form>
+    <>
+      <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+        <TextInput
+          size="sm"
+          prefix="ignite.com/"
+          placeholder="seu-usuário"
+          {...register('username')}
+        />
+        <Button size="sm" type="submit">
+          Reservar
+          <ArrowRight />
+        </Button>
+      </Form>
+      <FormAnnotation>
+        <Text size="sm">{errors.username && errors.username.message}</Text>
+      </FormAnnotation>
+    </>
   )
 }
