2331f691d2a10b981191199500c0a8b16a9244c3
Leonardo M. Primieri <leo.primieri@gmail.com>
2023-02-05 11:13:33 -0300

    feat: redirect user when flow is complete

--- src/pages/register/components/update-profile-form/update-profile-form.tsx
+++ src/pages/register/components/update-profile-form/update-profile-form.tsx
@@ -5,13 +5,16 @@ import { Avatar, Button, Text } from '@ignite-ui/react'
 import { GetServerSideProps } from 'next'
 import { unstable_getServerSession as getServerSideSession } from 'next-auth'
 import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
 import { ArrowRight } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { UpdateProfileFormOutput } from './update-profile-schema'
 import { FormAnnotation, ProfileBox } from './update-profile-styles'
 
 export default function UpdateProfileForm() {
-  const { data } = useSession()
+  const session = useSession()
+
+  const router = useRouter()
 
   const {
     register,
@@ -25,12 +28,17 @@ export default function UpdateProfileForm() {
     await api.put('/users/profile/update', {
       bio: formData.bio,
     })
+
+    await router.push(`/schedule/${session?.data?.user?.username}`)
   }
 
   return (
     <ProfileBox as="form" onSubmit={handleSubmit(handleUpdateProfile)}>
       <Text size="sm">Foto de perfil</Text>
-      <Avatar src={data?.user?.avatar_url} alt={data?.user?.name} />
+      <Avatar
+        src={session?.data?.user?.avatar_url}
+        alt={session?.data?.user?.name}
+      />
       <TextInput textArea {...register('bio')} label="Sobre você" />
       <FormAnnotation size="sm">
         Fale um pouco sobre você. Isto será exibido em sua página pessoal.
